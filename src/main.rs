7f56b571d01f3a300bbf51b48a90c3cbfe011b93
kula <kula@live.com>
2022-09-29 18:00:15 +0800

    feat: 🎸 use tag

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,20 @@ pub struct Image {
 }
 
 #[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[serde(rename_all = "lowercase", tag="MsgType")]
 pub enum Message {
     #[serde(rename_all = "PascalCase")]
-    ImageReplyMessage {
+    Image {
         to_user_name: String,
         from_user_name: String,
         create_time: i64,
-        msg_type: String,
         image: Image,
     },
 
     #[serde(rename_all = "PascalCase")]
-    StandardMessage {
+    Text {
         to_user_name: String,
         from_user_name: String,
-        create_time: u64,
-        msg_type: String,
-        content: String,
-        msg_id: u64,
-        msg_data_id: Option<String>,
-        idx: Option<i32>,
     },
 }
 
@@ -54,15 +48,12 @@ mod tests {
             <xml>
                 <ToUserName><![CDATA[gh_1e11f4ae566b]]></ToUserName>
                 <FromUserName><![CDATA[o-Wld6YNY-H801WBEiW-P9s1MzaU]]></FromUserName>
-                <CreateTime>1664344892</CreateTime>
                 <MsgType><![CDATA[text]]></MsgType>
-                <Content><![CDATA[@dd]]></Content>
-                <MsgId>23827690509052792</MsgId>
             </xml>
         "#;
-        let event: Xml = quick_xml::de::from_str(xml).unwrap();
-        match event.xml {
-            Message::StandardMessage { to_user_name, .. } => {
+        let event: Message = quick_xml::de::from_str(xml).unwrap();
+        match event {
+            Message::Text { to_user_name, .. } => {
                 assert!(to_user_name == "gh_1e11f4ae566b")
             }
             _ => assert!(false),
